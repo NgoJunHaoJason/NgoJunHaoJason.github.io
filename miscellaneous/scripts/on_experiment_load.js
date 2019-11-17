6d20ca0b88ee7344d0f98c29ee9da56a8b3a2f7b
Jason <c170127@e.ntu.edu.sg>
2019-11-17 11:44:18 +0800

    set canvas size to content div size

--- miscellaneous/scripts/on_experiment_load.js
+++ miscellaneous/scripts/on_experiment_load.js
@@ -2,13 +2,13 @@
 
 let displayCanvas = document.getElementById('displayCanvas');
 
-if (canvas) {
+if (displayCanvas) {
     let context = displayCanvas.getContext('2d');
     context.strokeText('Test', 10, 50);
 
     let contentDiv = document.querySelectorAll('.content')[0];
     console.log(contentDiv);
-    
+
     displayCanvas.width = contentDiv.offsetWidth;
     displayCanvas.height = contentDiv.offsetHeight;
 }
