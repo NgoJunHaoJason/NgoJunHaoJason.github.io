e72a9f0ed7b1410d927e6b775eb681199e97b400
Jason <c170127@e.ntu.edu.sg>
2019-11-17 12:45:05 +0800

    try fix black display on mobile

--- miscellaneous/scripts/on_experiment_load.js
+++ miscellaneous/scripts/on_experiment_load.js
@@ -1,11 +1,12 @@
 'use strict';
 
-// on load, show a black display
+// on load page, show a black display
+window.onload = function () {
+    let displayCanvas = document.getElementById('displayCanvas');
 
-let displayCanvas = document.getElementById('displayCanvas');
-
-if (displayCanvas) {
-    let context = displayCanvas.getContext('2d');
-    context.fillStyle = '#000000'; // black
-    context.fillRect(0, 0, displayCanvas.clientWidth, displayCanvas.height);
+    if (displayCanvas) {
+        let context = displayCanvas.getContext('2d');
+        context.fillStyle = '#000000'; // black
+        context.fillRect(0, 0, displayCanvas.clientWidth, displayCanvas.height);
+    }
 }
