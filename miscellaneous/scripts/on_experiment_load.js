5b537224b809065b6010a3ad8ba67ce27c38c271
Jason <c170127@e.ntu.edu.sg>
2019-11-17 12:51:01 +0800

    fix canvas width

--- miscellaneous/scripts/on_experiment_load.js
+++ miscellaneous/scripts/on_experiment_load.js
@@ -1,12 +1,11 @@
 'use strict';
 
 // on load page, show a black display
-window.onload = function () {
-    let displayCanvas = document.getElementById('displayCanvas');
+let displayCanvas = document.getElementById('displayCanvas');
 
-    if (displayCanvas) {
-        let context = displayCanvas.getContext('2d');
-        context.fillStyle = '#000000'; // black
-        context.fillRect(0, 0, displayCanvas.clientWidth, displayCanvas.height);
-    }
+if (displayCanvas) {
+    let context = displayCanvas.getContext('2d');
+    context.fillStyle = '#000000'; // black
+    context.fillRect(0, 0, displayCanvas.width, displayCanvas.height);
 }
+
