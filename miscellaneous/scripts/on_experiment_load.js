074107d57810e945253ef77ea0d27a3d1202c67e
Jason <c170127@e.ntu.edu.sg>
2019-11-17 11:57:15 +0800

    update display style

--- miscellaneous/scripts/on_experiment_load.js
+++ miscellaneous/scripts/on_experiment_load.js
@@ -5,10 +5,4 @@ let displayCanvas = document.getElementById('displayCanvas');
 if (displayCanvas) {
     let context = displayCanvas.getContext('2d');
     context.strokeText('Test', 10, 50);
-
-    let contentDiv = document.querySelectorAll('.content')[0];
-    console.log(contentDiv);
-
-    displayCanvas.width = contentDiv.offsetWidth;
-    displayCanvas.height = contentDiv.offsetHeight;
 }
