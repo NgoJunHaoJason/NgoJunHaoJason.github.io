d181848f93825486b90b6c619c6b496447d966cf
Jason <c170127@e.ntu.edu.sg>
2019-11-17 12:04:43 +0800

    show black display on load experimental page

--- miscellaneous/scripts/on_experiment_load.js
+++ miscellaneous/scripts/on_experiment_load.js
@@ -1,8 +1,11 @@
 'use strict';
 
+// on load, show a black display
+
 let displayCanvas = document.getElementById('displayCanvas');
 
 if (displayCanvas) {
     let context = displayCanvas.getContext('2d');
-    context.strokeText('Test', 10, 50);
+    context.fillStyle = '#000000'; // black
+    context.fillRect(0, 0, displayCanvas.clientWidth, displayCanvas.height);
 }
