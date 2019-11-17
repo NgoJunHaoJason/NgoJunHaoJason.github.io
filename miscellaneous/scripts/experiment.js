23533b6a920601ba2d8f8b3d7b9b60cddf67f204
Jason <c170127@e.ntu.edu.sg>
2019-11-17 16:43:01 +0800

    remove use of tensorflow in script

--- miscellaneous/scripts/experiment.js
+++ miscellaneous/scripts/experiment.js
@@ -16,22 +16,6 @@ const canvasHeight = displayCanvas.height;
 const video = document.getElementById('video');
 const startStopButton = document.getElementById('start-stop-button');
 
-// use tensorflow
-
-// not latest: https://cdn.jsdelivr.net/npm/@tensorflow/tfjs
-addScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest');
-addScripts('https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd');
-
-function addScripts(url) {
-    let script = document.createElement("script");
-    script.type = 'text/javascript';
-    script.src = url;
-    document.head.appendChild(script);
-}
-
-const model = async () => await cocoSsd.load();
-console.log(model);
-
 // add event handlers
 video.onloadeddata = drawCanvas;
 startStopButton.onclick = startCamera;
