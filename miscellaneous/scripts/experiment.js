7813b48d2b681601400a242854ac3f634f26ccb8
Jason <c170127@e.ntu.edu.sg>
2019-11-17 16:16:48 +0800

    fix typo

--- miscellaneous/scripts/experiment.js
+++ miscellaneous/scripts/experiment.js
@@ -16,7 +16,7 @@ const canvasHeight = displayCanvas.height;
 const video = document.getElementById('video');
 const startStopButton = document.getElementById('start-stop-button');
 
-const model = cocoSsd.load(config);
+const model = cocoSsd.load();
 console.log(model);
 
 // add event handlers
