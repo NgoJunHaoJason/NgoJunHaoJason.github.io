0dc58d753fe512015d96cd02364604b54f7a9834
Jason <c170127@e.ntu.edu.sg>
2019-11-17 13:10:38 +0800

    add start and stop buttons

--- miscellaneous/scripts/experiment.js
+++ miscellaneous/scripts/experiment.js
@@ -1,10 +1,32 @@
 'use strict';
 
-// https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+function startDisplay(displayId) {
+    let displayCanvas = document.getElementById(displayId);
 
-let constraints = {
-    video: {
-        width: { min: 1024, ideal: 1280, max: 1920 },
-        height: { min: 776, ideal: 720, max: 1080 }
+    if (displayCanvas) {
+        let context = displayCanvas.getContext('2d');
+        
+        context.font = '30px Arial';
+        context.strokeStyle = '#FFFFFF'; // white
+        context.strokeText('display stuff...', 5, 5);
     }
 }
+
+function stopDisplay(displayId) {
+    let displayCanvas = document.getElementById(displayId);
+
+    if (displayCanvas) {
+        let context = displayCanvas.getContext('2d');
+        context.fillStyle = '#000000'; // black
+        context.fillRect(0, 0, displayCanvas.width, displayCanvas.height);
+    }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+
+// let constraints = {
+//     video: {
+//         width: { min: 1024, ideal: 1280, max: 1920 },
+//         height: { min: 776, ideal: 720, max: 1080 }
+//     }
+// }
