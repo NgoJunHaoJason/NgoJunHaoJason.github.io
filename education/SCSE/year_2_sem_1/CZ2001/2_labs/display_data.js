1126d48fe4d1b70ead4f9a86da1f5785ec95723c
Jason <c170127@e.ntu.edu.sg>
2019-11-24 18:19:08 +0800

    reverse y domain

--- education/SCSE/year_2_sem_1/CZ2001/2_labs/display_data.js
+++ education/SCSE/year_2_sem_1/CZ2001/2_labs/display_data.js
@@ -37,7 +37,7 @@ d3.csv(
 
     // Add Y axis
     let y = d3.scaleLinear()
-        .domain([0, d3.max(data, (datum) => datum.timeTaken)])
+        .domain([d3.max(data, (datum) => datum.numKeyComparisons), 0])
         .range([height, 0]);
 
     svg.append('g')
@@ -51,6 +51,6 @@ d3.csv(
         .attr('stroke-width', 1.5)
         .attr('d', d3.line()
             .x((datum) => x(datum.arraySize))
-            .y((datum) => x(datum.timeTaken))
+            .y((datum) => x(datum.numKeyComparisons))
         );
 });
