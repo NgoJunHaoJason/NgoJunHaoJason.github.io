d697ff69faef8c7bdca37b74da5ae65f74080db0
Jason <c170127@e.ntu.edu.sg>
2019-11-24 16:56:20 +0800

    console log first datum

--- education/SCSE/year_2_sem_1/CZ2001/2_labs/display_data.js
+++ education/SCSE/year_2_sem_1/CZ2001/2_labs/display_data.js
@@ -1,1 +1,6 @@
-console.log(typeof(addLoadEvent));
+console.log(typeof (addLoadEvent));
+
+d3.csv('lab_3_csv/cz2001_lab3_merge_sort_random_results.csv')
+    .then(function(data) {
+        console.log(data[0]);
+    });
