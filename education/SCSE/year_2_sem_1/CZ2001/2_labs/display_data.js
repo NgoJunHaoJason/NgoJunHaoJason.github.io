d41ce7d6939fb1bbf9f764d55b0159797a1991ea
Jason <c170127@e.ntu.edu.sg>
2019-11-24 18:35:19 +0800

    display rest of the data

--- education/SCSE/year_2_sem_1/CZ2001/2_labs/display_data.js
+++ education/SCSE/year_2_sem_1/CZ2001/2_labs/display_data.js
@@ -1,44 +1,45 @@
 // reference: https://www.d3-graph-gallery.com/graph/line_basic.html
 
-// set the dimensions and margins of the graph
-const margin = { top: 60, right: 60, bottom: 60, left: 60 };
-const width = 960 - margin.left - margin.right;
-const height = 480 - margin.top - margin.bottom;
+// set the dimensions and MARGINs of the graph
+const MARGIN = { top: 60, right: 60, bottom: 60, left: 60 };
+const WIDTH = 960 - MARGIN.left - MARGIN.right;
+const HEIGHT = 480 - MARGIN.top - MARGIN.bottom;
 
-// append the svg object to the body of the page
-let svg = d3.select('#insertion_sort_ascending_results_visualisation')
+function displayData(divId, csvPath) {
+    // append the svg object to the body of the page
+    let svg = d3.select('#' + divId)
     .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
+    .attr('WIDTH', WIDTH + MARGIN.left + MARGIN.right)
+    .attr('HEIGHT', HEIGHT + MARGIN.top + MARGIN.bottom)
     .append('g')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    .attr('transform', 'translate(' + MARGIN.left + ',' + MARGIN.top + ')');
 
-d3.csv(
-    'lab_3_csv/cz2001_lab3_insertion_sort_ascending_results.csv',
-    function (datum) {
-        console.log(datum);
-        return {
-            arraySize: Number(datum['Array Size']),
-            numKeyComparisons: Number(datum['Number of Key Comparisons']),
-            timeTaken: Number(datum['CPU time taken for sorting']),
-        };
-    }
-).then(function (data) {
+    d3.csv(
+        csvPath,
+        function (datum) {
+            console.log(datum);
+            return {
+                arraySize: Number(datum['Array Size']),
+                numKeyComparisons: Number(datum['Number of Key Comparisons']),
+                timeTaken: Number(datum['CPU time taken for sorting']),
+            };
+        }
+    ).then(function (data) {
     // Add X axis --> it is a date format
     // .domain([0, d3.max(data, (datum) => datum.arraySize)])
     // .domain(d3.extent(data, (datum) => datum.arraySize))
     let x = d3.scaleLinear()
         .domain([0, d3.max(data, (datum) => datum.arraySize)])
-        .range([0, width]);
+        .range([0, WIDTH]);
 
     svg.append('g')
-        .attr('transform', 'translate(0,' + height + ')')
+        .attr('transform', 'translate(0,' + HEIGHT + ')')
         .call(d3.axisBottom(x));
 
     // Add Y axis
     let y = d3.scaleLinear()
         .domain([0, d3.max(data, (datum) => datum.numKeyComparisons)])
-        .range([height, 0]);
+        .range([HEIGHT, 0]);
 
     svg.append('g')
         .call(d3.axisLeft(y));
@@ -48,9 +49,44 @@ d3.csv(
         .datum(data)
         .attr('fill', 'none')
         .attr('stroke', 'steelblue')
-        .attr('stroke-width', 1.5)
+        .attr('stroke-WIDTH', 1.5)
         .attr('d', d3.line()
             .x((datum) => x(datum.arraySize))
             .y((datum) => y(datum.numKeyComparisons))
         );
-});
+    });
+}
+
+let displaySortingResults = function () {
+    displayData(
+        'insertion_sort_ascending_results_visualisation',
+        'lab_3_csv/cz2001_lab3_insertion_sort_ascending_results.csv'
+    );
+
+    displayData(
+        'insertion_sort_random_results_visualisation',
+        'lab_3_csv/cz2001_lab3_insertion_sort_random_results.csv'
+    );
+
+    displayData(
+        'insertion_sort_descending_results_visualisation',
+        'lab_3_csv/cz2001_lab3_insertion_sort_descending_results.csv'
+    );
+
+    displayData(
+        'merge_sort_ascending_results_visualisation',
+        'lab_3_csv/cz2001_lab3_merge_sort_ascending_results.csv'
+    );
+
+    displayData(
+        'merge_sort_random_results_visualisation',
+        'lab_3_csv/cz2001_lab3_merge_sort_random_results.csv'
+    );
+
+    displayData(
+        'merge_sort_descending_results_visualisation',
+        'lab_3_csv/cz2001_lab3_merge_sort_descending_results.csv'
+    );
+}
+
+addLoadEvent(displaySortingResults);
