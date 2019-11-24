3f72611b816b7980ef63206c00a4c59601f294c7
Jason <c170127@e.ntu.edu.sg>
2019-11-24 17:42:37 +0800

    remove x-axis svg transform

--- education/SCSE/year_2_sem_1/CZ2001/2_labs/display_data.js
+++ education/SCSE/year_2_sem_1/CZ2001/2_labs/display_data.js
@@ -31,7 +31,7 @@ d3.csv(
         .range([0, width]);
 
     svg.append('g')
-        .attr('transform', 'translate(0,' + height + ')')
+        // .attr('transform', 'translate(0,' + height + ')')
         .call(d3.axisBottom(x));
 
     // Add Y axis
