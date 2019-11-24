e79b6cb60be8513a5f746989f67665832c6e0ca5
Jason <c170127@e.ntu.edu.sg>
2019-11-24 17:04:30 +0800

    convert data from strings to integers

--- education/SCSE/year_2_sem_1/CZ2001/2_labs/display_data.js
+++ education/SCSE/year_2_sem_1/CZ2001/2_labs/display_data.js
@@ -2,5 +2,10 @@ console.log(typeof (addLoadEvent));
 
 d3.csv('lab_3_csv/cz2001_lab3_merge_sort_random_results.csv')
     .then(function(data) {
+        data.forEach(function(datum) {
+            datum['Array Size'] = Number(datum['Array Size']);
+            datum['Number of Key Comparisons'] = Number(datum['Number of Key Comparisons']);
+            datum['CPU time taken for sorting'] = Number(datum['CPU time taken for sorting']);
+        });
         console.log(data[0]);
     });
