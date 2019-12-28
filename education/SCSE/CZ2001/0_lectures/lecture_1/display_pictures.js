e7494b19c09dd3b7d34a9c76f09ff9dc8cc17b00
Jason <c170127@e.ntu.edu.sg>
2019-12-28 16:43:31 +0800

    display number sets

--- education/SCSE/CZ2001/0_lectures/lecture_1/display_pictures.js
+++ education/SCSE/CZ2001/0_lectures/lecture_1/display_pictures.js
@@ -3,10 +3,8 @@ displayFunctionPicture('function_picture');
 
 function displaySetsPicture(divId) {
     const svgWidth = 720;
-    const svgHeight = 240;
+    const svgHeight = 280;
 
-    const xOffset = 100;
-    const yOffset = 100;
     const circleRadius = 60;
 
     const setsPictureSVG = d3.select('#' + divId)
@@ -14,20 +12,19 @@ function displaySetsPicture(divId) {
         .attr('viewBox', '0 0 ' + svgWidth + ' ' + svgHeight)
         .attr('preserveAspectRatio', 'xMidYMid meet');
 
-    const alphabetSet = setsPictureSVG.append('g')
-        .attr('transform', `translate(${xOffset}, ${yOffset})`);
+    const alphabetGroup = setsPictureSVG.append('g')
+        .attr('transform', `translate(${100}, ${100})`);
 
-    alphabetSet.append('circle')
+    alphabetGroup.append('circle')
         .attr('r', circleRadius)
         .attr('fill', 'turquoise')
         .attr('stroke', 'black')
         .attr('stroke-width', 2);
 
-    alphabetSet.append('text')
-        .attr('text-anchor', 'center')
-        .attr('x', -circleRadius)
+    alphabetGroup.append('text')
+        .attr('text-anchor', 'middle')
         .attr('y', -circleRadius - 10)
-        .attr('font-size', 24)
+        .attr('font-size', 26)
         .text('alphabets');
 
     let alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('');
@@ -38,18 +35,47 @@ function displaySetsPicture(divId) {
         const alphabetIndex = Math.floor(Math.random() * alphabets.length)
         const alphabet = alphabets.splice(alphabetIndex, 1);
 
-        alphabetSet.append('text')
-            .attr('text-anchor', 'center')
-            .attr('x', -5)
+        alphabetGroup.append('text')
+            .attr('text-anchor', 'middle')
             .attr('y', 20 * index - 30)
             .text(alphabet);
     }
 
-    setsPictureSVG.append('text')
-        .attr('x', svgWidth - 300)
-        .attr('y', svgHeight - 20)
-        .attr('font-size', 24)
-        .text('TODO: add more examples');
+    const xRadius = 140;
+    const yRadius = 110;
+
+    const numberGroup = setsPictureSVG.append('g')
+        .attr('transform', `translate(${svgWidth * 2 / 3}, ${svgHeight / 2})`);
+
+    numberGroup.append('text')
+        .attr('text-anchor', 'middle')
+        .attr('y', -yRadius - 10)
+        .attr('font-size', 26)
+        .text('number sets');
+
+    const numberSets = ['real', 'rational', 'integer', 'natural'];
+
+    // reference: https://www.d3-graph-gallery.com/graph/custom_color.html
+    const colourScale = d3.scaleLinear()
+        .domain([1, numberSets.length])
+        .range(['cyan', 'blue']);
+
+    const groupEnter = numberGroup.selectAll('g').data(numberSets)
+        .enter().append('g');
+
+    groupEnter.append('ellipse')
+        .attr('cy', (datum, index) => index * 20)
+        .attr('rx', (datum, index) => xRadius - index * 20)
+        .attr('ry', (datum, index) => yRadius - index * 20)
+        .attr('fill', (datum, index) => colourScale(index + 1))
+        .attr('stroke', 'white');
+
+    groupEnter.append('text')
+        .attr('text-anchor', 'middle')
+        .attr('fill', 'white')
+        .attr('font-size', 26)
+        .attr('y', (datum, index) => index * 40 - yRadius + 30)
+        .text(datum => datum);
 }
 
 function displayFunctionPicture(divId) {
@@ -66,7 +92,7 @@ function displayFunctionPicture(divId) {
 
     const textGroup = functionPictureSVG.append('g')
         .attr('transform', `translate(${35}, ${30})`);
-    
+
     textGroup.append('text')
         .attr('x', -5)
         .attr('font-size', 28)
@@ -78,12 +104,12 @@ function displayFunctionPicture(divId) {
         .text('f(1) = A');
 
     textGroup.append('text')
-    .attr('y', 60)
+        .attr('y', 60)
         .attr('font-size', 24)
         .text('f(2) = C');
 
     textGroup.append('text')
-    .attr('y', 90)
+        .attr('y', 90)
         .attr('font-size', 24)
         .text('f(3) = C');
 
@@ -99,7 +125,7 @@ function displayFunctionPicture(divId) {
 
     for (let number = 0; number < 3; ++number) {
         domainGroup.append('text')
-            .attr('x', -5)
+            .attr('text-anchor', 'middle')
             .attr('y', number * 35 - 30)
             .attr('font-size', 22)
             .text(number + 1);
@@ -117,7 +143,7 @@ function displayFunctionPicture(divId) {
 
     for (let index = 0; index < 4; ++index) {
         rangeGroup.append('text')
-            .attr('x', -5)
+            .attr('text-anchor', 'middle')
             .attr('y', index * 30 - 40)
             .attr('font-size', 22)
             .text(String.fromCharCode(65 + index));
@@ -129,15 +155,15 @@ function displayFunctionPicture(divId) {
 
     functionPictureSVG.append('defs')
         .append('marker')
-            .attr('id', 'arrowhead')
-            .attr('refX', 5)
-            .attr('refY', 5)
-            .attr('markerWidth', 6)
-            .attr('marketHeight', 6)
-            .attr('orient', 'auto')
-            .attr('viewBox', '0 0 10 10')
-            .append('path')
-                .attr('d', 'M 0 0 L 10 5 L 0 10 z');
+        .attr('id', 'arrowhead')
+        .attr('refX', 5)
+        .attr('refY', 5)
+        .attr('markerWidth', 6)
+        .attr('marketHeight', 6)
+        .attr('orient', 'auto')
+        .attr('viewBox', '0 0 10 10')
+        .append('path')
+        .attr('d', 'M 0 0 L 10 5 L 0 10 z');
 
     functionPictureSVG.append('line')
         .attr('x1', svgWidth / 2 + 10)
