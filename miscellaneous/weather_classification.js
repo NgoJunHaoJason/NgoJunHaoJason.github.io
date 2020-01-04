b33fdfe4865b37b773ddd79854e4bee44d26fd6b
Jason <c170127@e.ntu.edu.sg>
2020-01-04 16:40:30 +0800

    rearrange code

--- miscellaneous/weather_classification.js
+++ miscellaneous/weather_classification.js
@@ -1,7 +1,6 @@
 const input = document.getElementById('input');
-input.onchange = function () { handleFiles(this); };
-
-function handleFiles(this) {
+input.onchange = function (this) { handleFiles(this); };
+input.addEventListener('change', function () {
     if (this.files && this.files[0]) {
         const image = document.createElement('img')
         image.src = URL.createObjectURL(this.files[0]);
@@ -14,4 +13,19 @@ function handleFiles(this) {
         console.log(this.files);
         return;
     }
-}
+})
+
+// function handleFiles() {
+//     if (this.files && this.files[0]) {
+//         const image = document.createElement('img')
+//         image.src = URL.createObjectURL(this.files[0]);
+
+//         const contentDiv = document.getElementsByClassName('content')[0];
+//         contentDiv.appendChild(image);
+//     }
+//     else {
+//         console.log('invalid file');
+//         console.log(this.files);
+//         return;
+//     }
+// }
