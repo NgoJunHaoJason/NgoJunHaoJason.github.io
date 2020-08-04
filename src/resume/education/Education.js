50d823c83cabd5492c96a8662285d08b8bd8d1d6
Jason <c170127@e.ntu.edu.sg>
2020-08-04 18:54:50 +0800

    add beautiful-skill-tree

--- src/resume/education/Education.js
+++ src/resume/education/Education.js
@@ -5,7 +5,46 @@ import {
     Header,
 } from 'semantic-ui-react';
 
+import {
+    SkillTreeGroup,
+    SkillTree,
+    SkillProvider,
+    SkillType,
+    SkillGroupDataType,
+} from 'beautiful-skill-tree';
+
 function Education() {
+    const data = [
+        {
+            id: 'hello-world',
+            title: 'Hello World',
+            tooltip: {
+                content:
+                    'This node is the top most level, and will be unlocked, and ready to be clicked.',
+            },
+            children: [
+                {
+                    id: 'hello-sun',
+                    title: 'Hello Sun',
+                    tooltip: {
+                        content:
+                            'This is a parent of the top node, and will locked while the parent isn’t in a selected state.',
+                    },
+                    children: [],
+                },
+                {
+                    id: 'hello-stars',
+                    title: 'Hello Stars',
+                    tooltip: {
+                        content:
+                            'This is the child of ‘Hello World and the sibling of ‘Hello Sun’. Notice how the app takes care of the layout automatically? That’s why this is called Beautiful Skill Tree and not just ‘Skill Tree’. (Also the npm namespace had already been taken for the latter so (flick hair emoji).',
+                    },
+                    children: [],
+                },
+            ],
+        },
+    ];
+
     return (
         <Container>
             <Header
@@ -16,9 +55,19 @@ function Education() {
                 </Header.Content>
             </Header>
 
-            <Container text>
-                Coming Soon
-            </Container>
+            {/* <SkillProvider>
+                <SkillTreeGroup>
+                    {({ skillCount }) => (
+                        <SkillTree
+                            treeId="tree"
+                            title="Skill Tree"
+                            data={data}
+                            collapsible
+                            description="My first skill tree"
+                        />
+                    )}
+                </SkillTreeGroup>
+            </SkillProvider> */}
         </Container>
     );
 }
