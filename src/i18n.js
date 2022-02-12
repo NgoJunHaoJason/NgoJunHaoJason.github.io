ace961185a04bd3d69979c69b04cefb52617e3d6
NgoJunHaoJason <njhjason@protonmail.com>
2022-02-12 17:04:23 +0800

    Detect browser language

--- src/i18n.js
+++ src/i18n.js
@@ -1,4 +1,5 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 import english from './assets/translations/english.json';
@@ -6,6 +7,7 @@ import chinese from './assets/translations/chinese.json';
 
 i18n
     .use(initReactI18next)
+    .use(LanguageDetector)
     .init({
         interpolation: { escapeValue: false },
         lng: 'en',
